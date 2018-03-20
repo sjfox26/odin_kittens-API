088b5873fcbe69e87fe539c51889053975dcafa0
Scott Fox <scottfox87@gmail.com>
2018-03-20 08:09:48 -0500

    generate kitten model and migrate database

---
+++ test/system/kittens_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class KittensTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit kittens_url
+  #
+  #   assert_selector "h1", text: "Kitten"
+  # end
+end
